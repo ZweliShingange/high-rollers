81cbd0dd91081f174a8b0998ddaaa8bcf4bc7307
billo-2021 <billomailbox@gmail.com>
2024-10-26 14:08:35 +0200

    feat(project-setup): add tui, prettier, and eslint packages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TuiRoot } from "@taiga-ui/core";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, TuiRoot],
+  template: `<tui-root><router-outlet /></tui-root>`,
 })
 export class AppComponent {
   title = 'high-rollers';
